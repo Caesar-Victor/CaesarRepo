0ad5b196489e71105f7e9e98d5a00377a7204b9b
CaesarVictor <carsarvictor@usp.br>
2021-04-21 19:40:26 -0300

    basc

--- basicoC/main.c
+++ basicoC/main.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
-void main() {
-    int a = 2;
-    int b = 3;
-    printf("%d\n", a+b);
+int main()
+{
+    int a, b;
+    char c;
+    printf("Digite a: ");
+    scanf("%d", &a);
+    printf("Digite b: ");
+    scanf("%d", &b);
+    do{
+        printf("Digite a operação(+, -, *, /, s para sair): ");
+        'scanf(" %c", &c);'
+        if (c == '+')
+        {
+            printf("a + b = %d\n", a + b);
+        }
+        else if (c == '-')
+        {
+            printf("a - b = %d\n", a - b);
+        }
+        else if (c == '*')
+        {
+            printf("a * b = %d\n", a * b);
+        }
+        else if (c == '/')
+        {
+            printf("a / b = %f\n", a / b);
+        }
+        else
+        {
+            printf("Operação inválida\n");
+        }
+    } while (c =!'s');
+
+    return 0;
 }
