cd90ccf9bbd25df8cabb674570b3cecd5e92a8c8
CaesarVictor <caesarvictor@usp.br>
2021-09-15 20:53:37 -0300

    aula03 check

--- imersaodev/aula03/codigo.js
+++ imersaodev/aula03/codigo.js
@@ -1,22 +1,38 @@
 var numSecreto = parseInt(Math.random()*10)
+var numChances = 3
 
 function Chutar() {
     var chute = parseInt(document.getElementById("valor").value);
     var resultado = document.getElementById("resultado")
-    if (chute == numSecreto) {
-        resultado.innerHTML = "Você acertou!!!"
-    } else if (chute < 0 || chute > 10) {
-        resultado.innerHTML = "Digite um valor válido"
-    } else {
-        resultado.innerHTML = "Errou"
+    var botao = document.getElementById("botao")
+    var acertou = false;
+    if(!acertou) {
+        if (chute == numSecreto) {
+            resultado.innerHTML = "Você acertou!!!"
+            acertou = true;
+        } else if (chute < 0 || chute > 10) {
+            resultado.innerHTML = "Digite um valor válido"
+        } else {
+            if (chute < numSecreto) {
+                resultado.innerHTML = "O numero secreto é maior <br/>"
+            } else {
+                resultado.innerHTML = "O numero secreto é menor <br/>"
+            }
+
+            resultado.innerHTML += "Você ainda tem " + --numChances + " tentativas"
+        }
+    }
+    if (numChances == 0) {
+        resultado.innerHTML = "Game Over!<br/>O número secreto era: "+numSecreto
+        numSecreto = parseInt(Math.random()*10)
     }
 }
 
 /* 01. Adicionar um número de tentativas para a pessoa tentar acertar e imprimir a resposta no final.
-
+    check
 
  02. Quando a pessoa errar, deixar na mensagem se o número chutado é maior ou menor que o número secreto.
-
+    check
 
  03. Pesquisar e aprender a diferença entre == e ===
 
