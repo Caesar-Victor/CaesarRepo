48b8e05796400dfb713b8aea6ae5364d697716ae
CaesarVictor <caesarvictor@usp.br>
2021-09-14 23:13:50 -0300

    atividade 2 completa

--- imersaodev/aula02/comandos.js
+++ imersaodev/aula02/comandos.js
@@ -18,17 +18,47 @@ function Converter(evento) {
         elemento.innerHTML = textoDeSaida;
     } else if (evento == "temperatura"){
         if (document.getElementById("c").checked){
-            textoDeSaida = "celso"
+            textoDeSaida = Celcius(valorElemento)
             elemento.innerHTML = textoDeSaida;
         }else if(document.getElementById("f").checked){
-            textoDeSaida = "jjuhifs"
+            textoDeSaida = Frht(valorElemento)
             elemento.innerHTML = textoDeSaida;
         }else if(document.getElementById("k").checked){
-            textoDeSaida = "kevin"
+            textoDeSaida = Kelvin(valorElemento)
             elemento.innerHTML = textoDeSaida;
         }
     }
 }
-function Temperatura() {
-    
+function Celcius(valor) {
+    var texto = "";
+    var valorFrht = 1.8*valor + 32
+    var valorKevin = valor + 273.15
+    if (document.getElementById("frht").checked){
+        texto += "O valor em Fahrenheit é: "+valorFrht.toFixed(4)+"<br/>"
+    }if (document.getElementById("kelvin").checked){
+        texto += "O valor em Kelvin é: "+valorKevin.toFixed(4)
+    }
+    return texto
+}
+function Frht(valor) {
+    var texto = "";
+    var valorCelcius = (valor - 32) / 1.8
+    var valorKevin = ((valor - 32) * (5/9)) + 273
+    if (document.getElementById("frht").checked){
+        texto += "O valor em Celcius é: "+valorCelcius.toFixed(4)+"<br/>"
+    }if (document.getElementById("kelvin").checked){
+        texto += "O valor em Kelvin é: "+valorKevin.toFixed(4)
+    }
+    return texto
+}
+function Kelvin(valor) {
+    var texto = "";
+    var valorCelcius = valor - 273.15
+    var valorFrht = ((valor - 273.15) * 1.8) + 32
+    if (document.getElementById("frht").checked){
+        texto += "O valor em Celcius é: "+valorCelcius.toFixed(4)+"<br/>"
+    }if (document.getElementById("kelvin").checked){
+        texto += "O valor em Kelvin é: "+valorFrht.toFixed(4)
+    }
+    return texto
 }
