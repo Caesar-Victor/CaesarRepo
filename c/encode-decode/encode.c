aa2b0b0bf5def82bdde3a2c687bd9ec675e8df0b
CaesarVictor <caesarvictor@usp.br>
2022-01-21 16:09:17 -0300

    main funcional

--- c/encode-decode/encode.c
+++ c/encode-decode/encode.c
@@ -14,40 +14,6 @@ void gera_raizes(const char *pass, unsigned char *r1, unsigned char *r2)
   }
 }
 
-void encode(FILE *arquivo, char *senha, char *nome)
-{
-  char car, ca;
-  int k = 0;
-  if (check(arquivo))
-  {
-    printf("Arquivo ja codificado");
-    return;
-  }
-  rewind(arquivo);
-  recebeSenha(senha);
-  char s1, s2;
-  gera_raizes(senha, &s1, &s2);
-  FILE *novo;
-  strcat(nome, ".enc");
-  novo = fopen(nome, "wb");
-  fprintf(novo, "ENCD%c%c", s1, s2);
-  int cont = 0;
-  while (!feof(arquivo))
-  {
-    ca = fgetc(arquivo);
-    if (cont % 2 == 0)
-    {
-      fprintf(novo, "%c", (ca + s1));
-    }
-    else if (cont % 2 != 0)
-    {
-      fprintf(novo, "%c", (ca + s2));
-    }
-    cont++;
-  }
-  fclose(novo);
-}
-
 bool check(FILE *arquivo){
   char car, ca, c1, c2, c3, c4;
   c1 = fgetc(arquivo);
@@ -74,19 +40,51 @@ void recebeSenha(char *senha){
   senha[k] = 0;
 } 
 
+void encode(FILE *arquivo, char *nome)
+{
+  char car, ca, senha[500];
+  int k = 0;
+  if (check(arquivo))  {
+    printf("Arquivo ja codificado");
+    return;
+  }
+  rewind(arquivo);
+  recebeSenha(senha);
+  char s1, s2;
+  gera_raizes(senha, &s1, &s2);
+  FILE *novo;
+  strcat(nome, ".enc");
+  novo = fopen(nome, "wb");
+  fprintf(novo, "ENCD%c%c", s1, s2);
+  int cont = 0;
+  while (!feof(arquivo))
+  {
+    ca = fgetc(arquivo);
+    if (cont % 2 == 0)
+    {
+      fprintf(novo, "%c", (ca + s1));
+    }
+    else if (cont % 2 != 0)
+    {
+      fprintf(novo, "%c", (ca + s2));
+    }
+    cont++;
+  }
+  fclose(novo);
+  remove(senha);
+}
+
 int main()
 {
   FILE *arquivo;
-  char nome[500], senha[500];
+  char nome[300];
   printf("Digite o nome do arquivo a ser codificado: ");
-  fgets(nome, sizeof(nome), stdin);
-
+  scanf("%s", nome);
   if ((arquivo = fopen(nome, "r")) != NULL)
-    encode(arquivo, senha, nome);
+    encode(arquivo, nome);
   else
     printf("Erro na leitura do arquivo!\n");
 
   fclose(arquivo);
   remove(nome);
-  remove(senha);
 }
