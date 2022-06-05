43ab9ffc2388d12e06538f5d2e2648cd23370a65
Caesar-Victor <caesarvictor@usp.br>
2022-06-05 19:36:18 -0300

    exe1 completo

--- NUMERICO_2022/ep2/exe1.py
+++ NUMERICO_2022/ep2/exe1.py
@@ -8,16 +8,30 @@ import numpy as np
 
 from le_arquivo import n
 
-#f(x) = 1
-def f(x):
-      return 1
+# f(x) = 1 cubo
 
-def integra():
-      #cubo de aresta 1, integral de 0 a 1 de 1.
-      n1 = n(int(input("Digite a precisão de nós (6, 8 ou 10): ")))
-      soma = 0
-      for i in n1:
-            soma += f(i[0])*i[1]
-      return soma
 
-print (integra())
+def f1(x):
+    return 1
+
+# f(x)= -x+1 trapesio
+
+
+def f2(x):
+    return -x+1
+
+
+def integra(x):
+    # cubo de aresta 1, integral de 0 a 1 de 1.
+    n1=n(x)
+    soma1 = 0
+    soma2 = 0
+    for i in n1:
+        soma1 += f1(i[0])*i[1]
+        soma2 += f2(i[0])*i[1]
+    return soma1, soma2
+
+
+x = int(input("Digite a precisão de nós (6, 8 ou 10): "))
+soma1, soma2 = integra(x)
+print("O volume calculado com n", x, " do cubo é: ", soma1, "\nO volume calculado com n", x, " do trapesio é: ", soma2)
