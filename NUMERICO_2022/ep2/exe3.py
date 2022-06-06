ff7eb018a55d518b2020394a945c94bcb03de1a0
Caesar-Victor <caesarvictor@usp.br>
2022-06-05 23:22:08 -0300

    exe3 completo

--- NUMERICO_2022/ep2/exe3.py
+++ NUMERICO_2022/ep2/exe3.py
@@ -16,7 +16,12 @@ def f1(x,y):
     return math.sqrt((((-y1*np.exp(y1/x1))/x1**2)**2)+((np.exp(y1/x1)/x1)**2)+1) * (0.2) * ((x**2-x**3)/2)
 
 
-def integra(x):
+def f2(x,y):
+    x1 = ((0.2)*x) + 0.3
+    y1 = ((x1**2-x1**3)/2)*y + ((x1**2+x1**3)/2)
+    return np.exp(y1/x1) * (0.2) * ((x**2-x**3)/2)
+
+def integraArea(x):
     n1 = n(x)
     F = 0
     soma = 0
@@ -28,7 +33,20 @@ def integra(x):
         soma += F*i[1]
     return soma
 
+def integraVolume(x):
+    n1 = n(x)
+    F = 0
+    soma = 0
+    v=0
+    for i in n1:
+        for j in range(len(n1)):
+            v=((i[0]**2-i[0]**3)/2)*n1[j][1] + ((i[0]**2+i[0]**3)/2)
+            F += f2(i[0], n1[j][0]) * v
+        soma += F*i[1]
+    return soma
+
 
 x = int(input("Digite a precisão de nós (6, 8 ou 10): "))
-soma1 = integra(x)
-print("A area da superfície descrita", x, " n é: ", soma1)
+soma1 = integraArea(x)
+soma2 = integraVolume(x)
+print("A area da superfície descrita", x, " n é: ", soma1, "\nO volume abaixo da regiao é ", soma2)
